15ab78e0ef774a68950c4bd026742cc2a9798b69
hrl <cxghrl@gmail.com>
2016-02-26 08:15:38 +0800

    add: pc, comments

--- src/pc.sv
+++ src/pc.sv
@@ -13,7 +13,7 @@ module pc(
     input clk,
     input clr, // set pc to 32'h0
     input [31:0] last_pc,
-    input [1:0] pc_inc, // 00: pc += 1; 01: pc += 1 + imme; 10: pc = reg/imme
+    input [1:0] pc_inc, // 00: pc += 1; 01: pc += 1 + imme; 10: pc = reg/imme, 11: halt
     input alu_branch_result, // 0: normal; 1: branch
     input [31:0] abs_addr,
     input [31:0] branch_addr,
