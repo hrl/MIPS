ec0103458a6bcdf14e217ccc3074fcdd8c8113ce
hrl <cxghrl@gmail.com>
2016-04-05 10:18:15 +0800

    fix: cpu, clean code

--- src/cpu.sv
+++ src/cpu.sv
@@ -59,6 +59,49 @@ module cpu(
         assign dm_cs = controls[`CON_MEM_CS];
         assign dm_rd = controls[`CON_MEM_RD];
     `endif
+
+    /* Wires */
+    wire [31:0] imme_extented;
+    wire [31:0] shamt_extented;
+    // pc ff
+    wire [31:0] pc_ff_next_pc;
+    wire [31:0] current_pc;
+    wire [31:0] cycle_count;
+    // ins mem
+    wire [15:0] ins_addr;
+    wire [31:0] ins;
+    // control
+    wire [`CON_MSB:`CON_LSB] controls;
+    wire eret;
+    // regfile
+    wire [4:0] reg_read1_num;
+    wire [4:0] reg_read2_num;
+    wire [4:0] reg_write_num;
+    wire [31:0] reg_write_data;
+    wire [31:0] reg_read1_data;
+    wire [31:0] reg_read2_data;
+    // alu
+    wire [31:0] alu_a;
+    wire [31:0] alu_b;
+    wire [31:0] alu_result;
+    wire alu_zero;
+    // pc calc
+    wire alu_branch_result;
+    wire [31:0] pc_abs_addr;
+    wire [31:0] pc_branch_addr;
+    wire [31:0] next_pc;
+    // cp0
+    wire [31:0] cp0_read_data;
+    wire cp0_pc_jump;
+    wire [31:0] cp0_pc_addr;
+    wire cp0_writeback_mask;
+    wire [31:0] cp0_status;
+    wire [31:0] cp0_epc;
+    wire cp0_interrupt;
+    // data mem
+    wire [7:0] dm_addr;
+    wire [31:0] dm_write_data;
+    wire [31:0] dm_read_data;
     
     /* TEMP Syscall Handle */
     wire _syscall;
@@ -87,12 +130,10 @@ module cpu(
     /* !END TEMP Syscall Handle */
     
     /* Global */
-    wire [31:0] imme_extented;
     assign imme_extented = 
         (controls[`CON_IMME_EXT] == `IMME_EXT_ZERO) ? {16'h0, ins[`INS_RAW_IMME]} :
         (controls[`CON_IMME_EXT] == `IMME_EXT_SIGN) ? {(ins[`INS_RAW_IMME_SIGN] == 1'b0) ? 16'h0000: 16'hffff, ins[`INS_RAW_IMME]} :
         32'h0;
-    wire [31:0] shamt_extented;
     assign shamt_extented = {27'h0, ins[`INS_RAW_SHAMT]};
 
     /* Program Counter Flip-Flop */
@@ -100,12 +141,12 @@ module cpu(
     // INPUT
     // in global: clk
     // in global: pc_clr
-    wire [31:0] pc_ff_next_pc;
+    // wire [31:0] pc_ff_next_pc;
     assign pc_ff_next_pc = next_pc;
     // in control: controls
     // OUTPUT
-    wire [31:0] current_pc;
-    wire [31:0] cycle_count;
+    // wire [31:0] current_pc;
+    // wire [31:0] cycle_count;
     //// MODULE
     pc_ff main_pc_ff(
         .clk(clk),
@@ -119,10 +160,10 @@ module cpu(
     /* Ins Memory */
     //// VAR
     // INPUT
-    wire [15:0] ins_addr;
+    // wire [15:0] ins_addr;
     assign ins_addr = current_pc[15:0];
     // OUTPUT
-    wire [31:0] ins;
+    // wire [31:0] ins;
     //// MODULE
     rom ins_memory(
         .addr(ins_addr),
@@ -135,8 +176,8 @@ module cpu(
     // INPUT
     // in im: ins
     // OUTPUT
-    wire [`CON_MSB:`CON_LSB] controls;
-    wire eret;
+    // wire [`CON_MSB:`CON_LSB] controls;
+    // wire eret;
     //// MODULE
     control main_control(
         .ins(ins),
@@ -147,23 +188,23 @@ module cpu(
     /* Register File */
     //// VAR
     // INPUT
-    wire [4:0] reg_read1_num;
+    // wire [4:0] reg_read1_num;
     assign reg_read1_num =
         (controls[`CON_REG_READ1_NUM] == `REG_READ1_NUM_RS) ? ins[`INS_RAW_RS] :
         (controls[`CON_REG_READ1_NUM] == `REG_READ1_NUM_RT) ? ins[`INS_RAW_RT] :
         5'h0;
-    wire [4:0] reg_read2_num;
+    // wire [4:0] reg_read2_num;
     assign reg_read2_num =
         (controls[`CON_REG_READ2_NUM] == `REG_READ2_NUM_RS) ? ins[`INS_RAW_RS] :
         (controls[`CON_REG_READ2_NUM] == `REG_READ2_NUM_RT) ? ins[`INS_RAW_RT] :
         5'h0;
-    wire [4:0] reg_write_num;
+    // wire [4:0] reg_write_num;
     assign reg_write_num =
         (controls[`CON_REG_WRITE_NUM] == `REG_WRITE_NUM_RT) ? ins[`INS_RAW_RT] :
         (controls[`CON_REG_WRITE_NUM] == `REG_WRITE_NUM_RD) ? ins[`INS_RAW_RD] :
         (controls[`CON_REG_WRITE_NUM] == `REG_WRITE_NUM_31) ? 5'h1f :
         5'h0;
-    wire [31:0] reg_write_data;
+    // wire [31:0] reg_write_data;
     assign reg_write_data =
         (controls[`CON_REG_WRITE_DATA] == `REG_WRITE_DATA_ALU) ? alu_result :
         (controls[`CON_REG_WRITE_DATA] == `REG_WRITE_DATA_DM) ? dm_read_data :
@@ -172,19 +213,16 @@ module cpu(
         32'h0;
     // in control: controls
     // in global: clk
-    // make simulator happy
-    wire _cp0_writeback_mask;
-    assign _cp0_writeback_mask = cp0_writeback_mask;
     // OUTPUT
-    wire [31:0] reg_read1_data;
-    wire [31:0] reg_read2_data;
+    // wire [31:0] reg_read1_data;
+    // wire [31:0] reg_read2_data;
     //// MODULE
     regfile main_regfile(
         .read1_num(reg_read1_num),
         .read2_num(reg_read2_num),
         .write_num(reg_write_num),
         .write_data(reg_write_data),
-        .write_en(controls[`CON_REG_WRITE_EN] & _cp0_writeback_mask),
+        .write_en(controls[`CON_REG_WRITE_EN] & cp0_writeback_mask),
         .clk(clk),
         .read1_data(reg_read1_data),
         .read2_data(reg_read2_data),
@@ -196,20 +234,20 @@ module cpu(
     //// VAR
     // INPUT
     // in control: controls
-    wire [31:0] alu_a;
+    // wire [31:0] alu_a;
     assign alu_a =
         (controls[`CON_ALU_A] == `ALU_A_REG) ? reg_read1_data:
         (controls[`CON_ALU_A] == `ALU_A_IMME) ? imme_extented:
         32'h0;
-    wire [31:0] alu_b;
+    // wire [31:0] alu_b;
     assign alu_b =
         (controls[`CON_ALU_B] == `ALU_B_REG) ? reg_read2_data:
         (controls[`CON_ALU_B] == `ALU_B_IMME) ? imme_extented:
         (controls[`CON_ALU_B] == `ALU_B_SHAMT) ? shamt_extented:
         32'h0;
     // OUTPUT
-    wire [31:0] alu_result;
-    wire alu_zero;
+    // wire [31:0] alu_result;
+    // wire alu_zero;
     //// MODULE
     alu main_alu(
         .op(controls[`CON_ALU_OP]),
@@ -225,27 +263,25 @@ module cpu(
     // in global: pc_clr
     // in control: controls
     // in pc_ff: current_pc
-    wire _cp0_pc_jump;
-    assign _cp0_pc_jump = cp0_pc_jump;
-    wire alu_branch_result;
+    // wire alu_branch_result;
     assign alu_branch_result =
         (controls[`CON_ALU_BRANCH] == `ALU_BRANCH_BEQ) ? alu_zero :
         (controls[`CON_ALU_BRANCH] == `ALU_BRANCH_BNE) ? !alu_zero :
         1'h0;
-    wire [31:0] pc_abs_addr;
+    // wire [31:0] pc_abs_addr;
     assign pc_abs_addr =
         (cp0_pc_jump == 1'b1) ? cp0_pc_addr :
         (controls[`CON_PC_JUMP] == `PC_JUMP_IMME) ? imme_extented :
         (controls[`CON_PC_JUMP] == `PC_JUMP_REG) ? reg_read1_data :
         32'h0;
-    wire [31:0] pc_branch_addr;
+    // wire [31:0] pc_branch_addr;
     assign pc_branch_addr = imme_extented;
     // OUTPUT
-    wire [31:0] next_pc;
+    // wire [31:0] next_pc;
     //// MODULE
     pc_calculator main_pc_calculator(
         .last_pc(current_pc),
-        .pc_inc(_cp0_pc_jump ? `PC_INC_JUMP : controls[`CON_PC_INC] | _syscall_pc_inc_mask),
+        .pc_inc(cp0_pc_jump ? `PC_INC_JUMP : controls[`CON_PC_INC] | _syscall_pc_inc_mask),
         .alu_branch_result(alu_branch_result),
         .abs_addr(pc_abs_addr),
         .branch_addr(pc_branch_addr),
@@ -261,13 +297,13 @@ module cpu(
     // in global: hardware_interrupt;
     // in control: eret;
     // OUTPUT
-    wire [31:0] cp0_read_data;
-    wire cp0_pc_jump;
-    wire [31:0] cp0_pc_addr;
-    wire cp0_writeback_mask;
-    wire [31:0] cp0_status;
-    wire [31:0] cp0_epc;
-    wire cp0_interrupt;
+    // wire [31:0] cp0_read_data;
+    // wire cp0_pc_jump;
+    // wire [31:0] cp0_pc_addr;
+    // wire cp0_writeback_mask;
+    // wire [31:0] cp0_status;
+    // wire [31:0] cp0_epc;
+    // wire cp0_interrupt;
     //// MODULE
     cp0 main_cp0(
         .clk(clk),
@@ -286,14 +322,14 @@ module cpu(
     /* Data Memory */
     //// VAR
     // INPUT
-    wire [7:0] dm_addr;
+    // wire [7:0] dm_addr;
     assign dm_addr = alu_result[9:2]; // ignore low bits
     // in control: controls
     // in global: clk
-    wire [31:0] dm_write_data;
+    // wire [31:0] dm_write_data;
     assign dm_write_data = reg_read2_data;
     // OUTPUT
-    wire [31:0] dm_read_data;
+    // wire [31:0] dm_read_data;
     //// module
     ram data_memory(
         .addr(dm_addr),
